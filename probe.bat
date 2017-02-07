fe68be60374171a9f87004529fd4c306e6492fe3
Scott <mauicormac@gmail.com>
2017-02-07 12:44:24 -0600

    Update probe.bat
    Updated formatting outfile

--- probe.bat
+++ probe.bat
@@ -9,35 +9,47 @@ echo " | |                         "
 echo " |_|                         "
 echo "          ver 0.1            "
 
-echo [+] Setup
-set /p DST=Set Output Directory (C:\stuff):
-echo [+] Creating Output Directory at %DST%
-rmdir /S %DST%
-mkdir %DST%
+echo [*] Setup
+set /p DST=Set Output Directory ex: (C:\stuff):
+echo [+] Creating Output Directory at %OUTFILE%
+rmdir /S %OUTFILE%
+mkdir %OUTFILE%
 
-echo **** SYSINFO/SERVICES/SESSIONS **** >> output.txt
-systeminfo >> output.txt
-sc query >> output.txt
-Tasklist /v >> output.txt
-Net Start >> output.txt
-Net Session >> output.txt
-nbtstat -S >> output.txt
-netstat -na >> output.txt
-echo **** shares and users **** >> output.txt
-net users >> output.txt
-net view >> output.txt
-net view \\127.0.0.1 >> output.txt
-echo **** REGISTRY **** >> output.txt
-Reg query HKLM\Software\Microsoft\Windows\CurrentVersion\Run >> output.txt
-Reg query HKLM\Software\Microsoft\Windows\CurrentVersion\RunOnce >> output.txt
-Reg query HKLM\Software\Microsoft\Windows\CurrentVersion\RunOnceEx >> output.txt
-Reg query HKCU\Software\Microsoft\Windows\CurrentVersion\Run >> output.txt
-Reg query HKCU\Software\Microsoft\Windows\CurrentVersion\Run >> output.txt
-Reg query HKCU\Software\Microsoft\Windows\CurrentVersion\RunOnceEx >> output.txt
-echo **** SCHEDULED TASKS/EVENTLOG **** >> output.txt
-at >> output.txt
-schtasks >> output.txt
-echo **** FILESYSTEM/LARGEFILES **** >> output.txt
-forfiles /S /M * /C "cmd /c if @fsize geq 1048576 echo @file  @path" >> output.txt
-echo "Script Complete"
+echo [*******************************************************************] >> %OUTFILE%
+echo [*] SYSTEM INFO / SERVICES / NETWORKING >> %OUTFILE%
+echo [>] Grabbing System Info
+systeminfo >> %OUTFILE%
+echo [>] Grabbing services 
+sc query >> %OUTFILE%
+echo [>] Grabbing Tasklist 
+Tasklist /v >> %OUTFILE%
+echo [>] Grabbing Network Sessions
+nbtstat -S >> %OUTFILE%
+echo [>] Grabbing TCP/IP Connections & PIDS
+netstat -ano >> %OUTFILE%
+echo [*******************************************************************] >> %OUTFILE%
+echo [*] USERS AND SHARES >> %OUTFILE%
+echo [>] Grabbing Users and SMB Shares
+net users >> %OUTFILE%
+net view >> %OUTFILE%
+net view \\127.0.0.1 >> %OUTFILE%
+echo [*******************************************************************] >> %OUTFILE%
+echo [*] REGISTRY >> %OUTFILE%
+echo [>] Grabbing Registry Information
+Reg query HKLM\Software\Microsoft\Windows\CurrentVersion\Run >> %OUTFILE%
+Reg query HKLM\Software\Microsoft\Windows\CurrentVersion\RunOnce >> %OUTFILE%
+Reg query HKLM\Software\Microsoft\Windows\CurrentVersion\RunOnceEx >> %OUTFILE%
+Reg query HKCU\Software\Microsoft\Windows\CurrentVersion\Run >> %OUTFILE%
+Reg query HKCU\Software\Microsoft\Windows\CurrentVersion\Run >> %OUTFILE%
+Reg query HKCU\Software\Microsoft\Windows\CurrentVersion\RunOnceEx >> %OUTFILE%
+echo [*******************************************************************] >> %OUTFILE%
+echo [>] Grabbing Scheduled Tasks
+echo [*] SCHEDULED TASKS >> %OUTFILE%
+at >> %OUTFILE%
+schtasks >> %OUTFILE%
+echo [*******************************************************************] >> %OUTFILE%
+echo [*] FILESYSTEM >> %OUTFILE%
+echo [>] Listing Files Larger than 100MB
+forfiles /S /M * /C "cmd /c if @fsize geq 104857600 echo @file  @path" >> %OUTFILE%
+echo [*] Complete
 pause
