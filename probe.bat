78e1834f83ce91740df2087bc7316591a4c46117
Scott <mauicormac@gmail.com>
2017-02-07 12:58:37 -0600

    Update probe.bat
    fixed a bunch of broken stuff

--- probe.bat
+++ probe.bat
@@ -9,45 +9,45 @@ echo " | |                         "
 echo " |_|                         "
 echo "          ver 0.1            "
 echo [*] Setup
-set /p OUTFILE=Set Output Directory ex: (C:\stuff):
-echo [+] Creating Output Directory at %OUTFILE%
-rmdir /S %OUTFILE%
-mkdir %OUTFILE%
+set /p OUTFILE=Set Output Path ex. (C:\Users\scott\):
+echo [+] Creating Output Directory at %OUTFILE%.
+rmdir /S %OUTFILE%probe_output
+mkdir %OUTFILE%probe_output
 
 echo [*******************************************************************] >> %OUTFILE%
-echo [*] SYSTEM INFO / SERVICES / NETWORKING >> %OUTFILE%
-echo [>] Grabbing System Info
-systeminfo >> %OUTFILE%
-echo [>] Grabbing services 
-sc query >> %OUTFILE%
-echo [>] Grabbing Tasklist 
-Tasklist /v >> %OUTFILE%
-echo [>] Grabbing Network Sessions
-nbtstat -S >> %OUTFILE%
-echo [>] Grabbing TCP/IP Connections / PIDS
-netstat -ano >> %OUTFILE%
+echo [>] Grabbing System Info.
+echo [*] SYSTEM INFO / SERVICES / NETWORKING >> %OUTFILE%probe_output\sysinfo.txt
+systeminfo >> %OUTFILE%probe_output\sysinfo.txt
+echo [>] Grabbing Services. 
+sc query >> %OUTFILE%probe_output\sysinfo.txt
+echo [>] Grabbing Tasklist.
+Tasklist /v >> %OUTFILE%probe_output\sysinfo.txt
+echo [>] Grabbing Network Sessions.
+nbtstat -S >> %OUTFILE%probe_output\sysinfo.txt
+echo [>] Grabbing TCP/IP Connections / PIDS.
+netstat -ano >> %OUTFILE%probe_output\sysinfo.txt
 echo [*******************************************************************] >> %OUTFILE%
-echo [*] USERS AND SHARES >> %OUTFILE%
-echo [>] Grabbing Users and SMB Shares
-net users >> %OUTFILE%
-net view >> %OUTFILE%
-net view \\127.0.0.1 >> %OUTFILE%
+echo [>] Grabbing Users and SMB Shares.
+echo [*] USERS AND SHARES >> %OUTFILE%probe_output\samba.txt
+net users >> %OUTFILE%probe_output\samba.txt
+net view >> %OUTFILE%probe_output\samba.txt
+net view \\127.0.0.1 >> %OUTFILE%probe_output\samba.txt
 echo [*******************************************************************] >> %OUTFILE%
-echo [*] REGISTRY >> %OUTFILE%
-echo [>] Grabbing Registry Information
-Reg query HKLM\Software\Microsoft\Windows\CurrentVersion\Run >> %OUTFILE%
-Reg query HKLM\Software\Microsoft\Windows\CurrentVersion\RunOnce >> %OUTFILE%
-Reg query HKLM\Software\Microsoft\Windows\CurrentVersion\RunOnceEx >> %OUTFILE%
-Reg query HKCU\Software\Microsoft\Windows\CurrentVersion\Run >> %OUTFILE%
-Reg query HKCU\Software\Microsoft\Windows\CurrentVersion\Run >> %OUTFILE%
-Reg query HKCU\Software\Microsoft\Windows\CurrentVersion\RunOnceEx >> %OUTFILE%
+echo [>] Grabbing Registry Information.
+echo [*] REGISTRY >> %OUTFILE%probe_output\registry.txt
+Reg query HKLM\Software\Microsoft\Windows\CurrentVersion\Run >> %OUTFILE%probe_output\registry.txt
+Reg query HKLM\Software\Microsoft\Windows\CurrentVersion\RunOnce >> %OUTFILE%probe_output\registry.txt
+Reg query HKLM\Software\Microsoft\Windows\CurrentVersion\RunOnceEx >> %OUTFILE%probe_output\registry.txt
+Reg query HKCU\Software\Microsoft\Windows\CurrentVersion\Run >> %OUTFILE%probe_output\registry.txt
+Reg query HKCU\Software\Microsoft\Windows\CurrentVersion\Run >> %OUTFILE%probe_output\registry.txt
+Reg query HKCU\Software\Microsoft\Windows\CurrentVersion\RunOnceEx >> %OUTFILE%probe_output\registry.txt
 echo [*******************************************************************] >> %OUTFILE%
-echo [>] Grabbing Scheduled Tasks
-echo [*] SCHEDULED TASKS >> %OUTFILE%
+echo [>] Grabbing Scheduled Tasks.
+echo [*] SCHEDULED TASKS >> %OUTFILE%probe_output\schtasks.txt
 schtasks >> %OUTFILE%
 echo [*******************************************************************] >> %OUTFILE%
-echo [*] FILESYSTEM >> %OUTFILE%
-echo [>] Listing Files Larger than 100MB
-forfiles /S /M * /C "cmd /c if @fsize geq 104857600 echo @file  @path" >> %OUTFILE%
-echo [*] Complete
+echo [>] Listing Files Larger than 100MB.
+echo [*] FILESYSTEM >> %OUTFILE%probe_output\files.txt
+forfiles /S /M * /C "cmd /c if @fsize geq 104857600 echo @file  @path" >> %OUTFILE%probe_output\files.txt
+echo [*] Complete.
 pause
